402dbecc77f40a645cfff2a71c3443f168356eec
AnneAbhiram <63452476+AnneAbhiram@users.noreply.github.com>
2021-11-08 23:55:26 +0530

    Add files via upload

---
+++ bolt_evm-code.js
@@ -0,0 +1,20 @@
+
+var Graph1 = new boltGraph();
+Graph1.setChartType("lineGraph");
+Graph1.setAxisName('X-Axis Name','Y-axis Name');
+Graph1.plotChart('time_stamp','person_a');
+
+var Graph2 = new boltGraph();
+Graph2.setChartType("lineGraph");
+Graph2.setAxisName('X-Axis Name','Y-axis Name');
+Graph2.plotChart('time_stamp','person_b');
+
+var Graph3 = new boltGraph();
+Graph3.setChartType("lineGraph");
+Graph3.setAxisName('X-Axis Name','Y-axis Name');
+Graph3.plotChart('time_stamp','person_c');
+
+var Graph3 = new boltGraph();
+Graph3.setChartType("lineGraph");
+Graph3.setAxisName('X-Axis Name','Y-axis Name');
+Graph3.plotChart('time_stamp','person_d');
